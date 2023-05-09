38ec764699c66c1c29d46c3e21aa18cf8412cc9e
Tom Overall <58610843+tomoverall@users.noreply.github.com>
2023-05-09 19:56:44 -0400

    Create CaesarCipher.java

---
+++ Spy Base/CaesarCipher.java
@@ -0,0 +1,10 @@
+public class CaesarCipher implements EncryptionBehaviour {
+
+    public String encrypt(String message, int encryptionKey) {
+        return message;
+    }
+
+    public String decrypt(String message, int encryptionKey) {
+        return message;
+    }
+}
